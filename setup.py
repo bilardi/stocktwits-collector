af24fcecdaf2ad69a718658914a6524217eeef9b
Alessandra Bilardi <alessandra.bilardi@gmail.com>
2022-04-04 23:28:46 +0200

    step 2 - added the package empty

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import stocktwits_collector
+
+setuptools.setup(
+    name="stocktwits-collector",
+    version=aws_saving.__version__,
+    author=aws_saving.__author__,
+    author_email="alessandra.bilardi@gmail.com",
+    description="Stocktwits collector Python package",
+    long_description=open('README.rst').read(),
+    long_description_content_type="text/x-rst",
+    url="https://stocktwits-collector.readthedocs.io/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    project_urls={
+        "Source":"https://github.com/bilardi/stocktwits-collector",
+        "Bug Reports":"https://github.com/bilardi/stocktwits-collector/issues",
+        "Funding":"https://donate.pypi.org",
+    },
+)
---
+++ stocktwits_collector/__init__.py
@@ -0,0 +1,19 @@
+"""Twits collector for Stocktwits
+
+This package contains the module to collect the twits of Stocktwits.
+
+It is part of educational repositories (https://github.com/pandle/materials)
+to learn how to write standard code and common uses of the TDD.
+
+Package contents one class to manage the collection features.
+
+    >>> import stocktwits_collector
+    >>> help(stocktwits_collector)
+    >>> import stocktwits_collector.collector as Collector
+    >>> help(Collector)
+
+# license MIT
+# support https://github.com/bilardi/stocktwits-collector/issues
+"""
+__version__ = '0.0.1'
+__author__ = 'Alessandra Bilardi'
